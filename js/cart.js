27520e90f963c052bc25ad2607422839869597dd
agustinsosa5 <agustinsosa2563@gmail.com>
2022-10-12 21:05:08 -0300

    consigna completada

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,3 @@
-
-
 async function carrito() {
     const respuesta = await fetch(`${CART_INFO_URL+ 25801 + EXT_TYPE}`);
     if (respuesta.ok){
@@ -8,19 +6,25 @@ async function carrito() {
         throw new Error(respuesta.status);
     }}; 
 
-    
+    async function change(){ 
+        const calculo = await carrito()
+        let cantprod = document.querySelector('#cantProd');   
+        subtotal.innerHTML =  `<b>USD: ${calculo.articles[0].unitCost * cantprod.value}`;}
+  
+
     document.addEventListener('DOMContentLoaded',async () =>{
         const imagen = document.querySelector('#imagen');        
         const nombre = document.querySelector('#nombre');
         const costo = document.querySelector('#cost');
-        let subtotal = document.querySelector('#subtotal');
-        let cantprod = document.querySelector('#cantProd');
-        
-        
+        let subtotal = document.querySelector('#subtotal');   
+                
         const compras = await carrito();        
         imagen.innerHTML = `<img width="100" src="${compras.articles[0].image}" alt="Autocargado">`;
         nombre.innerHTML = compras.articles[0].name
-        costo.innerHTML = 'USD: '+`${compras.articles[0].unitCost}`       
-        subtotal.innerHTML = `<b>USD: ${compras.articles[0].unitCost * cantprod.value}`
+        costo.innerHTML = 'USD: '+`${compras.articles[0].unitCost}`     
+
+
+
+
     });
     
